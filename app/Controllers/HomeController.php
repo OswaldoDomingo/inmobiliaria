c3cae7c3a88880d90796aad7664734e0e1fa4e0d
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2025-12-09 00:06:14 +0100

    ci:mostrar tarjeta -> false para no ver la tarjeta de entrada

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 
 /**
  * Controlador Principal
- * Gestiona la pÃ¡gina de inicio.
+ * Gestiona la página de inicio.
  */
 class HomeController
 {
@@ -20,10 +20,10 @@ class HomeController
         $heroTitle = "Encuentra tu hogar ideal";
         $heroSubTitle = "Miles de propiedades te esperan en Valencia";
         $heroImage = "https://picsum.photos/1920/600";
-        // --- LÃ LOGICA DE LA TARJETA ---
+        // --- LA LOGICA DE LA TARJETA ---
 
         // Preguntamos: ¿Existe la variable 'tarjeta_vista' en la sesión?
-        // El sÃ­mbolo '!' significa NO.
+        // El símbolo '!' significa NO.
         // Traducción: "Si NO está definida la variable 'tarjeta_vista'..."
 
         $mostrar_tarjeta = false; // Variable para mostrar la tarjeta de temporada o no
@@ -32,7 +32,7 @@ class HomeController
             $_SESSION['tarjeta_vista'] = true;
         } else {
             // Si ya existe la variable, es que ya entró antes
-            $mostrar_tarjeta = true;
+            $mostrar_tarjeta = false;
         }
         require VIEW . '/layouts/header.php';
         require VIEW . '/home.php';
